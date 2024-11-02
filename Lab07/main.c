218203d54b5e02d20af1b526065d5d7aef3d8415
MalexSw <sascha.040716@gmail.com>
2024-11-02 12:35:10 +0200

    Add Lab07

---
+++ Lab07/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int pyramidDrawing(int n) {
+    for (int amOfSymbols = 0; amOfSymbols < n; amOfSymbols++ ) {
+   for(int i = 0; i < n - amOfSymbols - 1; i++) {
+    printf(" ");
+   }
+  for(int m = 0; m < amOfSymbols * 2 + 1; m++) {
+    printf("+");
+  }
+    printf("\n");
+    }
+}
+
+int determinationForPrime(long n, long divisor) {
+    if (n <= 1) return 0;              
+    if (n <= 3) return 1;             
+    if (divisor * divisor > n) return 1; 
+    if (n % divisor == 0) return 0;     
+    return determinationForPrime(n, divisor + 1); 
+}
+
+int main() {
+    // long n = 74789380547;
+    long n = 11;
+    if (determinationForPrime(n, 2)) { 
+        printf("TRUE\n");
+    } else {
+        printf("FALSE\n");
+    }
+    printf("\n\n\n");
+    printf("----------------------------------------------------- \n\n\n");
+    pyramidDrawing(n);
+
+    return 0;
+}
