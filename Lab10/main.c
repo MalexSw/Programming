26c5e190950792520c7d8059776f814d845d56c7
MalexSw <sascha.040716@gmail.com>
2024-11-26 21:50:19 +0200

    Changed for getting arrays/matrixes as arguments

--- Lab10/main.c
+++ Lab10/main.c
@@ -2,119 +2,52 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-void diffBetweenDiag(int rows, int column) {
-    if (rows == column) {
-
-    int **matrix = (int **)malloc(rows * sizeof(int *));
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (int *)malloc(column * sizeof(int));
+// Function to generate a random array
+void generateRandomArray(int *array, int numberOfElements, int minValue, int maxValue) {
+    printf("Array:  \n");
+    for (int i = 0; i < numberOfElements; i++) {
+        array[i] = (rand() % (maxValue - minValue + 1)) + minValue;
+        printf("%d ", array[i]);
     }
+    printf("\n\n");
+}
 
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < column; c++) {
-            matrix[r][c] = (rand() % 100) + 1;
-            printf("%d  ", matrix[r][c]);
+// Function to generate a random matrix with multiple rows and columns
+void generateRandomMatrix(int **matrix, int rows, int columns, int minValue, int maxValue) {
+    printf("Matrix:  \n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            matrix[i][j] = (rand() % (maxValue - minValue + 1)) + minValue;
+            printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
-    printf("\n");
+    printf("\n\n");
+}
+
+// Function to calculate the sum difference between the main diagonal and second diagonal of a square matrix
+void diffBetweenDiag(int **matrix, int rows, int columns) {
+    if (rows != columns) {
+        printf("It's impossible to find diagonal values for a non-square matrix.\n");
+        return;
+    }
 
     int mainDiagSum = 0;
     int secondDiagSum = 0;
 
     for (int r = 0; r < rows; r++) {
-        mainDiagSum += matrix[r][r];             
-        secondDiagSum += matrix[r][column - r - 1]; 
+        mainDiagSum += matrix[r][r]; // Main diagonal
+        secondDiagSum += matrix[r][columns - r - 1]; // Secondary diagonal
     }
-
 
     printf("Main diagonal sum is %d \n", mainDiagSum);
-        printf("Second diagonal sum is %d \n", secondDiagSum);
-        printf("Difference between diagonals is %d \n", mainDiagSum - secondDiagSum);
-
-    for (int i = 0; i < rows; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-
-    } else {
-        printf("It's impossible to find diagonal value for matrix %d*%d", rows, column);
-    }
-
-    printf("\n");
+    printf("Second diagonal sum is %d \n", secondDiagSum);
+    printf("Difference between diagonals is %d \n", mainDiagSum - secondDiagSum);
 }
 
-
-
-void uniqueElemFinding(int rows, int columns) {
-
-    int **matrix = (int **)malloc(rows * sizeof(int *));
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (int *)malloc(columns * sizeof(int));
-    }
-
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < columns; c++) {
-            matrix[r][c] = (rand() % 100) + 1; 
-            printf("%d  ", matrix[r][c]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-    int size = rows * columns;
-    int *matrixArray = (int *)malloc(size * sizeof(int));
-    int *frequencyArray = (int *)malloc(size * sizeof(int));
-
-    for (int i = 0; i < size; i++) {
-        matrixArray[i] = matrix[i / columns][i % columns];
-        frequencyArray[i] = 1;
-    }
-
-    for (int i = 0; i < size; i++) {
-        if (frequencyArray[i] == 1) { 
-            for (int j = i + 1; j < size; j++) {
-                if (matrixArray[i] == matrixArray[j]) {
-                    frequencyArray[i]++;
-                    frequencyArray[j] = 0; 
-                }
-            }
-        }
-    }
-
-    printf("\n Unique elements in the matrix are: ");
-    for (int i = 0; i < size; i++) {
-        if (frequencyArray[i] == 1) {
-            printf("%d ", matrixArray[i]);
-        }
-    }
-    printf("\n");
-
-    for (int i = 0; i < rows; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-    free(matrixArray);
-    free(frequencyArray);
-
-    printf("\n");
-}
-
-
-
-void calcOfNegativeElementsAverage(int numberOfElements) {
-    int minValue = -10;
-    int maxValue = 15;
-    int sumOfNegNumbers = 0;
-    int amOfNegativeNum = 0;
-
-    int *array = (int *)malloc(numberOfElements * sizeof(int));
-
-    srand(time(0));
-    for (int i = 0; i < numberOfElements; i++) {
-        array[i] = (rand() % (maxValue - minValue + 1)) + minValue;
-    }
+// Function to calculate the average of negative elements in an array
+void calcOfNegativeElementsAverage(int *array, int numberOfElements) {
+    int sumOfNegNumbers = 0, amOfNegativeNum = 0;
 
     for (int i = 0; i < numberOfElements; i++) {
         if (array[i] < 0) {
@@ -136,31 +69,18 @@ void calcOfNegativeElementsAverage(int numberOfElements) {
         printf("In this array, there are no negative values\n");
     }
 
-    free(array);
-
     printf("\n");
 }
 
-void secondGreatest(int numberOfElements) {
-    int minValue = -10;
-    int maxValue = 15;
-    int theGreatest = minValue;
-    int secondGreatest = minValue;
-
-    int *array = (int *)malloc(numberOfElements * sizeof(int));
-
-    srand(time(0));
-    for (int i = 0; i < numberOfElements; i++) {
-        array[i] = (rand() % (maxValue - minValue + 1)) + minValue;
-    }
-
+// Function to find the second greatest element in an array
+void secondGreatest(int *array, int numberOfElements) {
     if (numberOfElements < 2) {
         printf("Array contains only one element: %d\n", array[0]);
-        free(array);
         return;
     }
 
-    theGreatest = array[0];
+    int theGreatest = array[0], secondGreatest = array[0];
+
     for (int i = 0; i < numberOfElements; i++) {
         if (array[i] > theGreatest) {
             secondGreatest = theGreatest;
@@ -177,108 +97,91 @@ void secondGreatest(int numberOfElements) {
     printf("\n");
     printf("Second greatest element is %d\n", secondGreatest);
 
-    free(array);
-
     printf("\n");
 }
 
-void arrayChanging(int numberOfElements) {
-    int minValue = -10;
-    int maxValue = 15;
-    char inputOperation = 'a'; // a for addition, s for subtraction, m for multiplying, and d for dividing
-    int valForModification = 3;
-
-    int *array = (int *)malloc(numberOfElements * sizeof(int));
-
-    srand(time(0));
-    for (int i = 0; i < numberOfElements; i++) {
-        array[i] = (rand() % (maxValue - minValue + 1)) + minValue;
-    }
-
+// Function to modify an array based on an operation
+void arrayChanging(int *array, int numberOfElements, char inputOperation, int valForModification) {
     printf("Old array:\n");
     for (int i = 0; i < numberOfElements; i++) {
         printf("%d ", array[i]);
     }
 
     switch (inputOperation) {
-    case 'a':
-        for (int i = 0; i < numberOfElements; i++) {
-            array[i] += valForModification;
-        }
-        break;
-    case 's':
-        for (int i = 0; i < numberOfElements; i++) {
-            array[i] -= valForModification;
-        }
-        break;
-    case 'm':
-        for (int i = 0; i < numberOfElements; i++) {
-            array[i] *= valForModification;
-        }
-        break;
-    case 'd':
-        if (valForModification != 0) {
+        case 'a':  // Addition
             for (int i = 0; i < numberOfElements; i++) {
-                array[i] /= valForModification;
+                array[i] += valForModification;
             }
-        } else {
-            printf("\nDivision by zero is not allowed.\n");
-            free(array);
+            break;
+        case 's':  // Subtraction
+            for (int i = 0; i < numberOfElements; i++) {
+                array[i] -= valForModification;
+            }
+            break;
+        case 'm':  // Multiplication
+            for (int i = 0; i < numberOfElements; i++) {
+                array[i] *= valForModification;
+            }
+            break;
+        case 'd':  // Division
+            if (valForModification != 0) {
+                for (int i = 0; i < numberOfElements; i++) {
+                    array[i] /= valForModification;
+                }
+            } else {
+                printf("\nDivision by zero is not allowed.\n");
+                return;
+            }
+            break;
+        default:
+            printf("\nInvalid operation.\n");
             return;
-        }
-        break;
-    default:
-        break;
     }
 
     printf("\nNew array:\n");
     for (int i = 0; i < numberOfElements; i++) {
         printf("%d ", array[i]);
     }
-    printf("\n");
-
-    free(array);
-
-    printf("\n");
+    printf("\n\n");
 }
 
-void sortedArraysMerging(int numberOfElements) {
-    int minValue = -10;
-    int maxValue = 15;
+// Function to find unique elements in a matrix
+void uniqueElemFinding(int **matrix, int rows, int columns) {
+    int size = rows * columns;
+    int *matrixArray = (int *)malloc(size * sizeof(int));
+    int *frequencyArray = (int *)malloc(size * sizeof(int));
 
-    int *array1 = (int *)malloc(numberOfElements * sizeof(int));
-    int *array2 = (int *)malloc(numberOfElements * sizeof(int));
-    int *mergedArray = (int *)malloc(2 * numberOfElements * sizeof(int));
-
-    srand(time(0));
-    for (int i = 0; i < numberOfElements; i++) {
-        array1[i] = (rand() % (maxValue - minValue + 1)) + minValue;
-        array2[i] = (rand() % (maxValue - minValue + 1)) + minValue;
+    for (int i = 0; i < size; i++) {
+        matrixArray[i] = matrix[i / columns][i % columns];
+        frequencyArray[i] = 1;
     }
 
-    for (int i = 0; i < numberOfElements - 1; i++) {
-        for (int j = 0; j < numberOfElements - i - 1; j++) {
-            if (array1[j] > array1[j + 1]) {
-                int temp = array1[j];
-                array1[j] = array1[j + 1];
-                array1[j + 1] = temp;
-            }
-            if (array2[j] > array2[j + 1]) {
-                int temp = array2[j];
-                array2[j] = array2[j + 1];
-                array2[j + 1] = temp;
+    for (int i = 0; i < size; i++) {
+        if (frequencyArray[i] == 1) {
+            for (int j = i + 1; j < size; j++) {
+                if (matrixArray[i] == matrixArray[j]) {
+                    frequencyArray[i]++;
+                    frequencyArray[j] = 0;
+                }
             }
         }
     }
 
-    printf("First array:\n");
-    for (int i = 0; i < numberOfElements; i++) {
-        printf("%d ", array1[i]);
+    printf("\nUnique elements in the matrix are: ");
+    for (int i = 0; i < size; i++) {
+        if (frequencyArray[i] == 1) {
+            printf("%d ", matrixArray[i]);
+        }
     }
-    printf("\nSecond array:\n");
-    for (int i = 0; i < numberOfElements; i++) {
-        printf("%d ", array2[i]);
-    }
+    printf("\n");
+
+    free(matrixArray);
+    free(frequencyArray);
+}
+
+// Function to merge two sorted arrays into a single sorted array
+void sortedArraysMerging(int *array1, int *array2, int numberOfElements) {
+    int *mergedArray = (int *)malloc(2 * numberOfElements * sizeof(int));
 
     int i = 0, j = 0, k = 0;
     while (i < numberOfElements && j < numberOfElements) {
@@ -302,28 +205,15 @@ void sortedArraysMerging(int numberOfElements) {
     }
     printf("\n");
 
-    free(array1);
-    free(array2);
     free(mergedArray);
-
-    printf("\n");
 }
 
-
-void evenAndOddNumbersDistributing(int numberOfElements) {
-    int minValue = -10;
-    int maxValue = 15;
-
-    int *array = (int *)malloc(numberOfElements * sizeof(int));
+// Function to distribute even and odd numbers into separate arrays
+void evenAndOddNumbersDistributing(int *array, int numberOfElements) {
     int *evenArray = (int *)malloc(numberOfElements * sizeof(int));
     int *oddArray = (int *)malloc(numberOfElements * sizeof(int));
 
     int evenCount = 0, oddCount = 0;
-
-    srand(time(0));
-    for (int i = 0; i < numberOfElements; i++) {
-        array[i] = (rand() % (maxValue - minValue + 1)) + minValue;
-    }
 
     printf("Original array:\n");
     for (int i = 0; i < numberOfElements; i++) {
@@ -346,22 +236,43 @@ void evenAndOddNumbersDistributing(int numberOfElements) {
     }
     printf("\n");
 
-    free(array);
     free(evenArray);
     free(oddArray);
-
-    printf("\n");
 }
 
-
+// Main function
 int main() {
     srand(time(NULL));
-     int numberOfElements = 10;
-    calcOfNegativeElementsAverage(numberOfElements);
-    secondGreatest(numberOfElements);
-    arrayChanging(numberOfElements);
-    sortedArraysMerging(numberOfElements);
-    evenAndOddNumbersDistributing(numberOfElements);
-    diffBetweenDiag(10, 10);
-    uniqueElemFinding(10, 10);
+
+    int numberOfElements = 10;
+    int rows = 5, columns = 5;
+    int minValue = -10, maxValue = 15;
+
+    int *array = (int *)malloc(numberOfElements * sizeof(int));
+    generateRandomArray(array, numberOfElements, minValue, maxValue);
+
+    // Matrix generation
+    int **matrix = (int **)malloc(rows * sizeof(int *));
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = (int *)malloc(columns * sizeof(int));
+    }
+    generateRandomMatrix(matrix, rows, columns, minValue, maxValue);
+
+    // Function calls
+    calcOfNegativeElementsAverage(array, numberOfElements);
+    secondGreatest(array, numberOfElements);
+    arrayChanging(array, numberOfElements, 'a', 5); // Example: addition with 5
+    sortedArraysMerging(array, array, numberOfElements); // Merging two same arrays
+    evenAndOddNumbersDistributing(array, numberOfElements);
+    uniqueElemFinding(matrix, rows, columns);
+    diffBetweenDiag(matrix, rows, columns);
+
+    // Clean up
+    free(array);
+    for (int i = 0; i < rows; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+
+    return 0;
 }
